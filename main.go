204d0ca57707c41b69db27234a313c106d07cd56
Jude Wood <judewoodprofessional@gmail.com>
2024-10-03 10:13:39 +0100

    Removed not needed wait group (#5)
    Co-authored-by: judewood <billvolestate@gmail.com>

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/judewood/bakery/models"
 	"github.com/judewood/bakery/orders"
@@ -10,8 +9,6 @@ import (
 	"github.com/judewood/bakery/store"
 	"github.com/judewood/bakery/utils"
 )
-
-var wg sync.WaitGroup
 
 func main() {
 	productStore := store.NewProductStore()
@@ -29,25 +26,19 @@ func main() {
 	fmt.Print(order.FormatOrder())
 
 	ch := make(chan models.ProductQuantity)
-	wg.Add(2)
 	go func() {
-		defer wg.Done()
 		for _, v := range order.Items {
 			ch <- v
 		}
 		close(ch)
 	}()
 
-	go func() {
-		defer wg.Done()
-		for v := range ch {
-			err := bakerService.Bake(v)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
+	for v := range ch {
+		err := bakerService.Bake(v)
+		if err != nil {
+			fmt.Println(err.Error())
 		}
-	}()
-	wg.Wait()
+	}
 
 	bakerService.Package()
 
